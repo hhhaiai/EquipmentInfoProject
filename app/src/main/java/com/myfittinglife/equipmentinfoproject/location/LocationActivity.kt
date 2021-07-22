33c62cc1932f0b88fcb30cf036008655b92b811b
ld <836354552@qq.com>
2021-07-22 22:54:19 +0800

    地理位置信息获取，补充注解

--- app/src/main/java/com/myfittinglife/equipmentinfoproject/location/LocationActivity.kt
+++ app/src/main/java/com/myfittinglife/equipmentinfoproject/location/LocationActivity.kt
@@ -31,14 +31,13 @@ class LocationActivity : AppCompatActivity() {
     companion object {
         const val TAG = "ceshi"
         const val LOCATION_REQUEST_CODE = 1
-        const val LISTENER_REQUEST_CODE=2
+        const val LISTENER_REQUEST_CODE = 2
     }
 
     private val permissionList =
         arrayOf(
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
-//            Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS
         )
 
 
@@ -62,7 +61,8 @@ class LocationActivity : AppCompatActivity() {
     //获取位置服务
     private lateinit var locationManager: LocationManager
 
-    var locationInfo = StringBuilder()
+    //用来在屏幕上显示位置信息
+    private val locationInfo = StringBuilder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,14 +72,31 @@ class LocationActivity : AppCompatActivity() {
 
         //获取位置信息
         btnLocationInfo.setOnClickListener {
+
+            //查看支持获取经纬度的方式有哪些
             var supportWay = StringBuilder()
             //passive：被动的从其他程序获取
-            for (provider in locationManager.getProviders(true)) {
-                supportWay.append(provider+"\t")
+            locationManager.getProviders(true).let {
+                if (it.size > 0) {
+                    for (provider in it) {
+                        supportWay.append(provider + "\t")
+                    }
+                    locationInfo.append("支持的获取经纬度的方式有:$supportWay").append("\n")
+                }
             }
-            locationInfo.append(supportWay).append("\n")
             Log.i(TAG, "支持的获取经纬度的方式有: $supportWay")
-            getLocationInfo()
+
+            //（一、申请权限）
+            if (PermissionUtil.requestPermission(LOCATION_REQUEST_CODE, permissionList.toList(), this)) {
+                //获取经纬度信息
+                getLocationInfo()
+            }
+
+
+        }
+        //设置监听器，周期性获取定位信息
+        btnGetCycleLocation.setOnClickListener {
+            locationMonitor()
         }
         //跳转到系统设置处来打开权限
         btnOpenSetting.setOnClickListener {
@@ -88,29 +105,28 @@ class LocationActivity : AppCompatActivity() {
         }
         //判断谷歌服务是否支持
         btnGoogleSupport.setOnClickListener {
-            if (isSupportGoogleService()) {
-                Toast.makeText(this, "支持谷歌服务", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "不支持谷歌服务", Toast.LENGTH_SHORT).show()
-            }
-        }
-        //设置监听器，周期性获取定位信息
-        btnGetCycleLocation.setOnClickListener {
-            locationMonitor()
+           isSupportGoogleService()
         }
     }
 
 
-
-
-
-    //----------------------------------------------------------------------------------------------
     /**
-     * 判断是否支持谷歌服务
+     * 查看手机是否支持谷歌服务
      */
-    private fun isSupportGoogleService(): Boolean {
-        var result = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
-        return result == ConnectionResult.SUCCESS
+    private fun isSupportGoogleService() {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        var resultCode = apiAvailability.isGooglePlayServicesAvailable(this)
+
+        //关闭谷歌基础服务返回SERVICE_DISABLED，开启谷歌基础服务返回SUCCESS
+        Log.i(TAG, "isSupportGoogleService: $resultCode")
+
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                Log.i(TAG, "isSupportGoogleService: 人为可以解决的错误")
+                GoogleApiAvailability.getInstance().getErrorDialog(this, resultCode, 200).show();
+            }
+        }
+
     }
 
     /**
@@ -128,53 +144,48 @@ class LocationActivity : AppCompatActivity() {
      */
     @SuppressLint("MissingPermission")
     fun getLocationInfo() {
-        if (isSupportGoogleService()) {
-            //申请权限（一）
-            if (PermissionUtil.requestPermission(LOCATION_REQUEST_CODE, permissionList.toList(), this)) {
-                //判断是否开启位置服务（二）
-                if (isLocationServiceOpen()) {
-                    //获取所有支持的provider（三）
-                    var providers = locationManager.getProviders(true)
-                    //存储最优的结果
-                    var betterLocation: Location? = null
-                    for (provider in providers) {
-                        val location = locationManager.getLastKnownLocation(provider)
-                        location?.let {
-                            Log.i(TAG, "$provider 精度为：${it.accuracy}")
-                            locationInfo.append("$provider 精度为：${it.accuracy} \n")
-                            if (betterLocation == null){
-                                betterLocation = it
-                            }else{
-                                //因为半径等于精度，所以精度越低代表越准确
-                                if (it.accuracy < betterLocation!!.accuracy)
-                                    betterLocation = it
-                            }
-                        }
-                        if (location==null){
-                            Log.i(TAG, "$provider 获取到的位置为null")
-                            locationInfo.append("$provider 获取到的位置为null \n")
-                        }
+
+        //判断是否开启位置服务（二）
+        if (isLocationServiceOpen()) {
+            //获取所有支持的provider（三）
+            var providers = locationManager.getProviders(true)
+            //存储最优的结果
+            var betterLocation: Location? = null
+            for (provider in providers) {
+                val location = locationManager.getLastKnownLocation(provider)
+                location?.let {
+                    Log.i(TAG, "$provider 精度为：${it.accuracy}")
+                    locationInfo.append("$provider 精度为：${it.accuracy} \n")
+                    if (betterLocation == null) {
+                        betterLocation = it
+                    } else {
+                        //因为半径等于精度，所以精度越低代表越准确
+                        if (it.accuracy < betterLocation!!.accuracy)
+                            betterLocation = it
                     }
-                    betterLocation?.let {
-                        Log.i(TAG, "精度最高的获取方式：${it.provider} 经度：${it.longitude}  纬度：${it.latitude}")
-                        locationInfo.append("精度最高的获取方式：${it.provider} 经度：${it.longitude}  纬度：${it.latitude} \n")
-                    }
-                    //（四）
-                    // TODO: 2021/7/2 如果最优的获取到的都是空，则开启连续定位服务
-                    if(betterLocation==null){
-                        for (provider in locationManager.getProviders(true)) {
-                            locationMonitor(provider)
-                        }
-                        Log.i(TAG, "getLocationInfo: 获取到的经纬度均为空，开启连续定位监听")
-                        locationInfo.append("getLocationInfo: 获取到的经纬度均为空，开启连续定位监听 \n")
-                    }
-                } else {
-                    Toast.makeText(this, "请跳转到系统设置中打开定位服务", Toast.LENGTH_SHORT).show()
+                }
+                if (location == null) {
+                    Log.i(TAG, "$provider 获取到的位置为null")
+                    locationInfo.append("$provider 获取到的位置为null \n")
                 }
             }
+            betterLocation?.let {
+                Log.i(TAG, "精度最高的获取方式：${it.provider} 经度：${it.longitude}  纬度：${it.latitude}")
+                locationInfo.append("精度最高的获取方式：${it.provider} 经度：${it.longitude}  纬度：${it.latitude} \n")
+            }
+            //（四）若所支持的定位获取方式获取到的都是空，则开启连续定位服务
+            if (betterLocation == null) {
+                for (provider in locationManager.getProviders(true)) {
+                    locationMonitor(provider)
+                }
+                Log.i(TAG, "getLocationInfo: 获取到的经纬度均为空，已开启连续定位监听")
+                locationInfo.append("getLocationInfo: 获取到的经纬度均为空，已开启连续定位监听 \n")
+            }
+            tvLocationInfo.text = locationInfo
         } else {
-            Toast.makeText(this, "该手机不支持谷歌服务", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "请跳转到系统设置中打开定位服务", Toast.LENGTH_SHORT).show()
         }
+
     }
 
     /**
@@ -204,14 +215,16 @@ class LocationActivity : AppCompatActivity() {
             )
         }
     }
+
     @SuppressLint("MissingPermission")
-    fun locationMonitor(provider:String){
+    fun locationMonitor(provider: String) {
         if (PermissionUtil.requestPermission(
                 LISTENER_REQUEST_CODE,
                 permissionList.toList(),
                 this
             )
         ) {
+            Log.i(TAG, "locationMonitor: 开启了连续定位$provider")
             locationManager.requestLocationUpdates(
                 provider,
                 3000.toLong(),
@@ -265,7 +278,7 @@ class LocationActivity : AppCompatActivity() {
                     Toast.makeText(this, "没有权限", Toast.LENGTH_SHORT).show()
                 }
             }
-            LISTENER_REQUEST_CODE->{
+            LISTENER_REQUEST_CODE -> {
                 if (PermissionUtil.verifyResult(grantResults, this)) {
                     locationMonitor()
                 } else {
